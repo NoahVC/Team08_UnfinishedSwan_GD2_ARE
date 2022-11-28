09fb2c90fbe92aaa646b974182b522d2f6882325
NoahVC <noah.van.crombruggen@student.howest.be>
2022-11-28 11:43:44 +0100

    ADD PaintSplash logic

--- Assets/Scripts/Player/Painting/PaintSplash.cs
+++ Assets/Scripts/Player/Painting/PaintSplash.cs
@@ -9,14 +9,42 @@ using UnityEngine;
 public class PaintSplash : MonoBehaviour
 {
     [SerializeField]
-    private IActivatable[] _iActivatable;
+    private float _timer = 5;
 
-    public void start()
+    private bool _deathStart = false;
+    [SerializeField]
+    private Vector3 _minScale;
+    private Vector3 _maxScale;
+    [SerializeField]
+    private float _speed = 2f;
+    private float _duration = 5f;
+    private float i;
+    private float _rate;
+
+    private void Start()
     {
-        foreach (IActivatable item in _iActivatable)
+        i = 0.0f;
+        _rate = (1.0f / _duration) * _speed;
+        _maxScale = transform.localScale;
+    }
+
+    private void Update()
+    {
+        if (_timer > 0)
         {
-            item.Activate();
+            _timer -= Time.deltaTime;
+            //Debug.Log("Tick tock " + _timer);
         }
+        if (_timer <= 0)
+            _deathStart = true;
+        if (_deathStart)
+        {
+            i += Time.deltaTime * _rate;
+            gameObject.transform.localScale = Vector3.Lerp(_maxScale, _minScale, i);
+        }
+        if (gameObject.transform.localScale == _minScale)
+            Destroy(gameObject);
+            
     }
 }
 
